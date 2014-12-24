f8b4488b6690f0b717434aa05564753d09a4a670
Mikhail Gerasimov <mgerasim@inbox.ru>
2014-12-24 14:25:08 +0900

    Session

--- TgappWeb/TgappWeb/Controllers/RadiusController.cs
+++ TgappWeb/TgappWeb/Controllers/RadiusController.cs
@@ -25,7 +25,7 @@ namespace TgappWeb.Controllers
         public ActionResult GetAccessByLogin(string filial, string login, string datebgn, string dateend)
         {
             var radiusClient = new RadiusAsync.radiusClient("http");
-
+            
             RadiusAsync.Get_Access_by_LoginResult theLoginResult;
             theLoginResult = radiusClient.Get_Access_by_Login(new Get_Access_by_LoginRequest()
             {
@@ -46,6 +46,30 @@ namespace TgappWeb.Controllers
             ViewBag.ResultSet = theLoginResult.ResultSet;
 
 
+            return View();
+        }
+
+        public ActionResult GetSessionsByLogin(string filial, string login, string datebgn, string dateend)
+        {
+            var radiusClient = new RadiusAsync.radiusClient("http");
+            RadiusAsync.Get_Sessions_by_LoginResult theSessionResult;
+            RadiusAsync.Get_Sessions_by_LoginRequest theSessionRequest = new RadiusAsync.Get_Sessions_by_LoginRequest();
+            theSessionRequest.Command = new MetaCommand()
+            {
+                Operation = DbOperation.ExecuteQuery
+            };
+            theSessionRequest.Connection = new MetaConnection() { Connection = "*.*" };
+            theSessionRequest.Parameters = new Get_Sessions_by_LoginInputParameters()
+            {
+                p_Date_Beg = DateTime.Parse(datebgn),
+                p_Date_End = DateTime.Parse(dateend),
+                p_Login = login,
+                p_Filial = filial
+            };
+
+
+            theSessionResult = radiusClient.Get_Sessions_by_Login(theSessionRequest);
+            ViewBag.ResultSet = theSessionResult.ResultSet;
             return View();
         }
 
