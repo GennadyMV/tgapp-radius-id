00d67a5c354aa0158dab8edf5b4367346cd7359a
Mikhail Gerasimov <mgerasim@inbox.ru>
2014-12-25 12:17:46 +0900

    support_IE

--- TgappWeb/TgappWeb/Scripts/radius.js
+++ TgappWeb/TgappWeb/Scripts/radius.js
@@ -7,21 +7,21 @@
 
 function Validation() {	
 	var isOk = true;
-	if (document.getElementById("login").value === '') {
+	if (document.getElementById("login").value === '' || document.getElementById("login").value === 'Введите логин') {
 		isOk = false;
 		$('#alert-danger-login').show();
 	}
 	else {
 		$('#alert-danger-login').hide();
 	}
-	if (document.getElementById("datebgn").value === '') {
+	if (document.getElementById("datebgn").value === '' || document.getElementById("datebgn").value === 'Дата начало периода') {
 		isOk = false;
 		$('#alert-danger-datebgn').show();
 	}
 	else {
 		$('#alert-danger-datebgn').hide();
 	}
-	if (document.getElementById("dateend").value === '') {
+	if (document.getElementById("dateend").value === '' || document.getElementById("dateend").value === 'Дата конца периода') {
 		isOk = false;
 		$('#alert-danger-dateend').show();
 	}
